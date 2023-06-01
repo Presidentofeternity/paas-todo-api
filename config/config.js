6bc86b1358d62a8bfae92bea3f2cd45d3552924f
presidentofeternity <87862843+Presidentofeternity@users.noreply.github.com>
2023-06-01 16:47:17 +0200

    seccondcommit

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 const { parse } = require("pg-connection-string")
-
+console.log(process.env.DATABASE_URL)
 const { host, port, database, user, password } = parse(process.env.DATABASE_URL)
 
 module.exports = {
